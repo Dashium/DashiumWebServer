45bece06077945e62c64fb8d5126a4eacc894fc4
Tai Studio <tai.studio@outlook.fr>
2022-01-12 16:10:14 +0100

    update

--- main.js
+++ main.js
@@ -3,17 +3,19 @@ var http = require('http');
 var serveIndex = require('serve-index');
 var serveStatic = require('serve-static');
 const fs = require('fs');
+const socketio = require('socket.io');
+
 const config = require('./config.json');
 
-if(!fs.existsSync(config.public)){
+if (!fs.existsSync(config.public)) {
   fs.mkdirSync(config.public);
 }
 
-var index = serveIndex(config.public, {'icons': true})
+var index = serveIndex(config.public, { 'icons': true });
 
-var serve = serveStatic(config.public)
+var serve = serveStatic(config.public);
 
-var server = http.createServer(function onRequest(req, res){
+const server = http.createServer(function onRequest(req, res) {
   var done = finalhandler(req, res)
   serve(req, res, function onNext(err) {
     if (err) return done(err)
@@ -21,4 +23,10 @@ var server = http.createServer(function onRequest(req, res){
   })
 })
 
-server.listen(config.port)
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log(`Connecté au client ${socket.id}`);
+})
+
+server.listen(config.port);
